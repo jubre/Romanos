b559678cbf0c1b8fe503cbaff141d24157e29e26
Raul Rodriguez <raulrodriguez782@gmail.com>
2011-11-20 22:59:01 -0500

    init Coding dojo romanos

---
+++ conversor.php
@@ -0,0 +1,36 @@
+<?php 
+class Conversor
+{
+  public function numberToRoman($number)
+ {
+
+	if($number<=3){
+		$romano = "";
+		for( $i=0; $i<$number; $i++)
+			$romano .="I";
+		return $romano;
+		
+	}
+    if($number == 4){
+      return "IV";
+    }
+
+	if($number == 5){
+      return "V";
+    }
+
+	if( $number < 9 && $number >= 6){
+		$romano = "V";
+        var_export($romano);
+        var_export($number);
+		$diff = $number - 5;
+		for ($i=0; $i<$diff; $i++)
+     {
+      		$romano .="I"; 
+     }
+		return $romano;
+ 	}
+	
+}
+}
+?>
